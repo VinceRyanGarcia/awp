aece22de64572425f5a96576dd828493cc2d1801
Vince Garcia <vinceryangarcia@gmail.com>
2021-06-23 15:22:24 -0700

    youtube update

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
         <center><img src={Pic1} alt="cover"/></center>
       </div>
 
-      <div>
-      <center><iframe className="Youtube" src="https://www.youtube.com/embed/ysz5S6PUM-U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+      <div className="YoutubeContainer">
+      <center><iframe className="Youtube" src="https://www.youtube.com/embed/3WLcI1uSMdI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
       </div>
 
       <div className="Container1">
