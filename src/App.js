f1d36884174b7a9f9ad7accbd9f537fb761d7eef
Vince Garcia <vinceryangarcia@gmail.com>
2021-06-23 11:48:57 -0700

    new version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import './App.css';
-import Navigation from './Navigation/Navigation'
-import Cover from './Cover/Cover'
-import Player from './Player/Player'
-import Footer from './Footer/Footer'
-import styled from 'styled-components'
+import awp from './images/awp.jpg'
+
 
 
 
 function App() {
   return (
     <div className="App">
+
+      <div className="Cover">
+        <img src={awp} alt="cover"></img>
+      </div>
+
+      <div className="Container1">
+
+        <div className="Box">
+        <h1>Box 1</h1>
+        </div>
+
+        <div className="Box">
+        <h1>Box 2</h1>
+        </div>
+
+      </div>
+
+      <div className="Container2">
+        <h1>Container 2</h1>
+      </div>
       
-        {/* <Navigation></Navigation> */}
-          <Cover></Cover>
-          <Player></Player>
-        <Footer></Footer>
-      
+      <div className="Container2">
+        <h1>Container 3</h1>
+      </div>
+
+      <div className="Footer">
+        <h1>Social</h1>
+        <a href="no_target">facebook</a>
+        <a href="no_target">instagram</a>
+        <a href="no_target">twitter</a>
+        <a href="no_target">youtube</a>
+        <a href="no_target">linkedin</a>
+
+        <h1>Contact</h1>
+        <p>awp@awperations.com</p>
+      </div>
     </div>
   );
 }
