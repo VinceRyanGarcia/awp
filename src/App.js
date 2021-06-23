8924fe9cc50ccb2a660df363b629bc1aec55faf9
Vince Garcia <vinceryangarcia@gmail.com>
2021-06-23 12:27:12 -0700

    blue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import awp from './images/awp.jpg'
+import ReactPlayer from 'react-player'
 
 
 
@@ -15,22 +16,25 @@ function App() {
       <div className="Container1">
 
         <div className="Box">
-        <h1>Box 1</h1>
+        <h1>Embrace My Passion</h1>
+        <p>Taking music to the fullest potential, to invoke emotions in those who never knew they had them.</p>
         </div>
 
         <div className="Box">
-        <h1>Box 2</h1>
+        <h1>Live Life in Beat</h1>
+        <p>I followed my dreams in playing across nightclubs in Los Angeles. Next, I'm headed for festivals around the world. </p>
         </div>
-
+        
+        <div className="Box">
+        <h1>Be Inspired by Everything</h1>
+        <p>I find inspiration all around me.  I try to make my music representative of myself and my generation. I love the places that most people don't give a second glance.</p>
+        </div>
       </div>
 
       <div className="Container2">
-        <h1>Container 2</h1>
       </div>
       
-      <div className="Container2">
-        <h1>Container 3</h1>
-      </div>
+    
 
       <div className="Footer">
         <h1>Social</h1>
